cf659c8d0685e69c96c671c7cff3938ad6d9d019
keshav-gupta14 <kgups2006@gmail.com>
2024-12-09 21:28:32 +0530

    Update assignments.ts

--- src/data/assignments.ts
+++ src/data/assignments.ts
@@ -17,8 +17,18 @@ export const assignments: Assignment[] = [
     type: 'PDF',
     driveLink: 'https://drive.google.com/file/d/12fNUryoyM9uU-5TLa4wQ4uibdoahAWLN/preview',
     description:
-      'The image is a promotional advertisement by "The Pijja Co." for their Ragi Pizza Base.',
+      'The image is a promotional advertisement by "The Pijja Co." for their Ragi Pizza Base.
+        Keshav Gupta 2024BCH1043',
     createdAt: '2023-12-02',
+    resources: [],
+  },
+  {
+    id: '3',
+    title: 'How to Book a Bus Ticket Online',
+    type: 'Video',
+    driveLink: 'https://youtube.com/shorts/1JBcy0AVZyA?si=qhR-GNHqGxL6gxdZ',
+    description: 'A short video tutorial on booking a bus ticket online.',
+    createdAt: '2023-12-09',
     resources: [],
   },
 ];
