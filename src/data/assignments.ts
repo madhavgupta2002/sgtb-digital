f4702a984fae90ccece6e0a2904ea97bbe51f3d1
keshav-gupta14 <kgups2006@gmail.com>
2024-12-17 19:04:52 +0530

    Update assignments.ts

--- src/data/assignments.ts
+++ src/data/assignments.ts
@@ -26,7 +26,8 @@ export const assignments: Assignment[] = [
     title: 'How to Book Bus Tickets Online',
     type: 'YT',
     driveLink: 'https://www.youtube.com/shorts/1JBcy0AVZyA',
-    description: 'Tutorial showing the step-by-step process of booking bus tickets online using DMRC Momentum 2.0 Application',
+    description:
+      'Tutorial showing the step-by-step process of booking bus tickets online using DMRC Momentum 2.0 Application',
     createdAt: '2024-11-30',
     resources: [],
   },
@@ -35,7 +36,8 @@ export const assignments: Assignment[] = [
     title: 'Project Report',
     type: 'PDF',
     driveLink: 'https://drive.google.com/file/d/1STBiHaXW5thS8uK1MkU_NpzwzBf7-Cm9/preview',
-    description: 'Report on a survey conducted on digital literacy intertwined with e-commerce',
+    description:
+      'Report on a survey conducted on digital literacy intertwined with e-commerce',
     createdAt: '2024-12-13',
     resources: [
       {
@@ -43,7 +45,8 @@ export const assignments: Assignment[] = [
         fileName: 'Digital Literacy Questionnaire',
         fileType: 'PDF',
         fileSize: 0,
-        fileUrl: 'https://drive.google.com/file/d/1y6xKwVnfBWAGuxJSIc9pm5xWl3Kguyn-/view',
+        fileUrl:
+          'https://drive.google.com/file/d/1y6xKwVnfBWAGuxJSIc9pm5xWl3Kguyn-/view',
         uploadDate: '2024-12-13',
         description: 'Digital Literacy Questionnaire',
       },
@@ -54,7 +57,8 @@ export const assignments: Assignment[] = [
     title: 'Digital India Mission',
     type: 'PDF',
     driveLink: 'https://drive.google.com/file/d/1R0qkOkF3y0xeVHTZGVZ6JDhsMs_od7a5/preview',
-    description: 'An informative document outlining the objectives and achievements of the Digital India Mission.',
+    description:
+      'An informative document outlining the objectives and achievements of the Digital India Mission.',
     createdAt: '2024-12-13',
     resources: [],
   },
@@ -63,7 +67,8 @@ export const assignments: Assignment[] = [
     title: 'Cyber Hygiene',
     type: 'PDF',
     driveLink: 'https://drive.google.com/file/d/1EDN7h_IzZQGAxLrlTibTo2QVBbEcoY3s/preview',
-    description: 'Guide on best practices to maintain cyber hygiene for secure online interactions.',
+    description:
+      'Guide on best practices to maintain cyber hygiene for secure online interactions.',
     createdAt: '2024-12-13',
     resources: [],
   },
@@ -72,7 +77,18 @@ export const assignments: Assignment[] = [
     title: 'Cyber Safety',
     type: 'PDF',
     driveLink: 'https://drive.google.com/file/d/13ckndlGAI9owU6uXDbhjx4b4AKEPxbp1/preview',
-    description: 'Educational material on protecting personal information and staying safe online.',
+    description:
+      'Educational material on protecting personal information and staying safe online.',
+    createdAt: '2024-12-13',
+    resources: [],
+  },
+  {
+    id: '8',
+    title: 'How to Book a Train Ticket On Make my Trip',
+    type: 'YT',
+    driveLink: 'https://www.youtube.com/shorts/-h2-Y5_GLJY',
+    description:
+      'How to Book a Train Ticket On Make my Trip',
     createdAt: '2024-12-13',
     resources: [],
   },
